ad71a7ea10c9009df8cf4e1489dd139872b748dd
Pedro Domingos <domingos@student.dei.uc.pt>
2024-02-20 15:41:44 +0000

    nova pasta projeto

---
+++ Backend/cesar-pedro-proj3/src/main/java/cesarpedroproj3/dto/User.java
@@ -0,0 +1,116 @@
+package aor.paj.project2.backend.dto;
+
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement
+public class User {
+    @XmlElement
+    private String username;
+    @XmlElement
+    private String password;
+    @XmlElement
+    private String email;
+    @XmlElement
+    private String firstName;
+    @XmlElement
+    private String lastName;
+    @XmlElement
+    private String phone;
+    @XmlElement
+    private String photoURL;
+    @XmlElement
+    private ArrayList<Task> userTasks = new ArrayList<>(); //ser array de ids das tasks assim as tasks ficavam no json das tasks
+
+    public User() {
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Task> getUserTasks() {
+        return userTasks;
+    }
+
+    public void setUserTasks(ArrayList<Task> userTasks) {
+        this.userTasks = userTasks;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", photoURL='" + photoURL + '\'' +
+                ", userTasks=" + userTasks +
+                '}';
+    }
+}
---
+++ Backend/cesar-pedro-proj3/src/main/java/cesarpedroproj3/service/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package aor.paj.project2.backend.service;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ApplicationConfig extends Application {
+}
