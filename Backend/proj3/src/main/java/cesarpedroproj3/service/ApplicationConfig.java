a1106c409b6d040b13e8fd947633c826e3d731a9
Pedro Domingos <domingos@student.dei.uc.pt>
2024-02-20 15:43:22 +0000

    atualizar

--- Backend/proj3/src/main/java/cesarpedroproj3/service/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package aor.paj.project2.backend.service;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class ApplicationConfig extends Application {
-}
