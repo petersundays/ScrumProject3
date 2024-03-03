d5db72634ea475f3f162e4aa8b95d33c6432eb4e
Pedro Domingos <domingos@student.dei.uc.pt>
2024-03-03 12:19:46 +0000

    simplificação do codigo na criação de asides para search e categorias

--- Frontend/home.js
+++ Frontend/home.js
@@ -78,6 +78,7 @@ function productOwnerPage(){
   liElement.appendChild(addUsersButton);
   document.getElementById('menu').appendChild(liElement);
   createSearchMenu();
+  createCategoriesMenu();
 }
 
   function cleanAllTaskFields() {
@@ -497,8 +498,27 @@ function createSearchMenu() {
   searchNav.addEventListener('click', function() {
     if (!asideCreated) { 
       removeAsideElements();
-      createAsideElements();
+      createAsideSearchTasks();
       asideCreated = true; 
+    }
+  });
+}
+
+function createCategoriesMenu() {
+  const categoriesNav = document.createElement('li');
+  categoriesNav.id = 'nav-categories';
+  const categoriesLink = document.createElement('a');
+  categoriesLink.innerText = "Categories";
+  categoriesNav.appendChild(categoriesLink);
+  document.getElementById('menu').appendChild(categoriesNav);
+
+  let asideCategoriesCreated = false;
+
+  categoriesNav.addEventListener('click', function() {
+    if (!asideCategoriesCreated) {
+      removeAsideElements();
+      createAsideCategories();
+      asideCategoriesCreated = true;
     }
   });
 }
@@ -516,13 +536,40 @@ function removeAsideElements() {
 
 }
 
-function createAsideElements() {
+function createAsideSearchTasks() {
   
   const asideElement = document.querySelector('aside');
   const asideTitle = document.createElement('h3');
   asideTitle.innerText = "Search Tasks";
   asideElement.appendChild(asideTitle);
 
+  createUserSearchElements(asideElement);
+  createUserSearchEventListeners();
+  createCategoriesElements(asideElement);
+  createCategoriesEventListeners();
+  createDeletedTasksElements(asideElement);
+  createDeletedTasksEventListeners();
+  
+}
+
+function createAsideCategories() {
+  createCategoriesElements(document.querySelector('aside'));
+}
+
+
+
+function createDefaultOption(dropdown, textContent) {
+  dropdown.innerHTML = '';
+  const defaultOption = document.createElement('option');
+  defaultOption.value = '';
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  defaultOption.hidden = true;
+  defaultOption.textContent = textContent;
+  dropdown.appendChild(defaultOption);
+}
+
+function createUserSearchElements(asideElement) {
   const userLabel = document.createElement('label');
   userLabel.innerText = "User";
   userLabel.id = 'label-search-user';
@@ -544,41 +591,17 @@ function createAsideElements() {
   searchUserButton.id = 'search-user-button';
   searchUserButton.innerText = "Search User's Tasks";
   asideElement.appendChild(searchUserButton);
+}
 
-  const categoryLabel = document.createElement('label');
-  categoryLabel.innerText = "Category";
-  categoryLabel.id = 'label-search-category';
-  asideElement.appendChild(categoryLabel);
+function createUserSearchEventListeners() {
 
-  const categoryInput = document.createElement('input');
-  categoryInput.type = 'text';
-  categoryInput.id = 'search-category-home';
-  categoryInput.placeholder = 'Category';
-  asideElement.appendChild(categoryInput);
-
-  const dropdownCategories = document.createElement('select');
-  dropdownCategories.id = 'categoriesType-home';
-  dropdownCategories.name = 'categoriesType';
-  asideElement.appendChild(dropdownCategories);
-
-  const searchCategoryButton = document.createElement('button');
-  searchCategoryButton.id = 'search-category-button';
-  searchCategoryButton.innerText = 'Search Category';
-  asideElement.appendChild(searchCategoryButton);
-
-  const deletedTasksLabel = document.createElement('label');
-  deletedTasksLabel.innerText = "Deleted Tasks";
-  deletedTasksLabel.id = 'label-search-deleted';
-  asideElement.appendChild(deletedTasksLabel);
-
-  const deletedTasksButton = document.createElement('button');
-  deletedTasksButton.id = 'search-deleted-button';
-  deletedTasksButton.innerText = 'Deleted Tasks';
-  asideElement.appendChild(deletedTasksButton);
+  const usernameInput = document.getElementById('search-input-home');
+  const dropdownUsers = document.getElementById('usersType-home');
+  const searchUserButton = document.getElementById('search-user-button');
+  const notAssigned = 'notAssigned';
 
   dropdownUsers.addEventListener('click', async function() {
-    const drodpDownUsersMessage = 'Select User';
-    const notAssigned = 'notAssigned';
+  
     if (usernameInput.value === '') {
       dropdownUsers.innerHTML = ''; 
       populateDropdownUsers(tokenValue);
@@ -621,6 +644,38 @@ function createAsideElements() {
     }
     createDefaultOption(dropdownUsers, 'Select User');
   });
+}
+
+
+
+function createCategoriesElements(asideElement) {
+  const categoryLabel = document.createElement('label');
+  categoryLabel.innerText = "Category";
+  categoryLabel.id = 'label-search-category';
+  asideElement.appendChild(categoryLabel);
+
+  const categoryInput = document.createElement('input');
+  categoryInput.type = 'text';
+  categoryInput.id = 'search-category-home';
+  categoryInput.placeholder = 'Category';
+  asideElement.appendChild(categoryInput);
+
+  const dropdownCategories = document.createElement('select');
+  dropdownCategories.id = 'categoriesType-home';
+  dropdownCategories.name = 'categoriesType';
+  asideElement.appendChild(dropdownCategories);
+
+  const searchCategoryButton = document.createElement('button');
+  searchCategoryButton.id = 'search-category-button';
+  searchCategoryButton.innerText = 'Search Category';
+  asideElement.appendChild(searchCategoryButton);
+
+}
+
+function createCategoriesEventListeners() {
+  const categoryInput = document.getElementById('search-category-home');
+  const dropdownCategories = document.getElementById('categoriesType-home');
+  const searchCategoryButton = document.getElementById('search-category-button');
 
   dropdownCategories.addEventListener('click', async function() {
     const drodpDownCategoriesMessage = 'Select Category';
@@ -664,7 +719,23 @@ function createAsideElements() {
     }
     createDefaultOption(dropdownCategories, 'Select Category');
   });
-  
+}
+
+function createDeletedTasksElements(asideElement) {
+  const deletedTasksLabel = document.createElement('label');
+  deletedTasksLabel.innerText = "Deleted Tasks";
+  deletedTasksLabel.id = 'label-search-deleted';
+  asideElement.appendChild(deletedTasksLabel);
+
+  const deletedTasksButton = document.createElement('button');
+  deletedTasksButton.id = 'search-deleted-button';
+  deletedTasksButton.innerText = 'Deleted Tasks';
+  asideElement.appendChild(deletedTasksButton);
+}
+
+function createDeletedTasksEventListeners() {
+  const deletedTasksButton = document.getElementById('search-deleted-button');
+
   deletedTasksButton.addEventListener('click', async function() {
     let userLogged = await getUser(tokenValue);
     const typeOfUser = userLogged.typeOfUser;
@@ -678,21 +749,8 @@ function createAsideElements() {
       attachDragAndDropListeners(taskElement);
     });
   });
-  
 }
 
-
-
-function createDefaultOption(dropdown, textContent) {
-  dropdown.innerHTML = '';
-  const defaultOption = document.createElement('option');
-  defaultOption.value = '';
-  defaultOption.disabled = true;
-  defaultOption.selected = true;
-  defaultOption.hidden = true;
-  defaultOption.textContent = textContent;
-  dropdown.appendChild(defaultOption);
-}
 
 
   function createTaskElement(task, typeOfUser) {
